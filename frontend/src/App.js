714b4f5a25cf8a7860b4f75e6ebd3eed3899b7af
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2024-01-30 17:56:13 +0200

    Frontend <> Updated App File to Display Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Routing from './routing';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Skills from './pages/skills/Skills';
+import Projects from './pages/projects/Projects';
+import Experience from './pages/experience/Experience';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Experience/>
     </div>
   );
 }
