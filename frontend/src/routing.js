7ee73f0235a4ae0af239c752125966a057e03da0
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2024-01-30 15:05:35 +0200

    Frontend <> Imported Pages to Routing

--- frontend/src/routing.js
+++ frontend/src/routing.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Skills from './pages/skills/Skills';
+import Projects from './pages/projects/Projects';
+import Experience from './pages/experience/Experience';
 
 const Routing = () => {
     return (
