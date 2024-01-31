165ba788800a51af05718f701fe631017954c792
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2024-01-31 12:09:29 +0200

    Frontend <> Updated Navbar Logo Style

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import './navbar.css';
+import '../../base.css';
 
 const Navbar = () => {
-    return (
-        <div className="navbar-container">
-            <div className="logo-container">
-                <div className="logo">Logo</div>
-            </div>
-            <nav className='navbar'>Navigation</nav>
-            <div className="cta-container">CTA Button</div>
-        </div>
-        
-    );
-}
- 
+	return (
+		<div className="main-container">
+			<div className="navbar-container">
+				<div className="logo-container">
+					<div className="logo font-big">
+						<span className="gold-text bolder">&#47;&#47;M</span>ohamad &nbsp;
+						<span className="gold-text bolder">R</span>ida
+					</div>
+				</div>
+				<nav className="navbar">Navigation</nav>
+				<div className="cta-container">CTA Button</div>
+			</div>
+		</div>
+	);
+};
+
 export default Navbar;
